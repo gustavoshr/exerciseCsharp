919eab6a6433c6cfb81b03e419ef8c1ecf2222d0
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 18:57:50 -0300

    exercise

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1 = 5;
+            double numero2 = 6;
+            double numero3 = 4; 
+            double numero4 = 3;
+            double numero5 = 2;
+            double soma = numero1 + numero2 + numero3 + numero4 + numero5;
+            Console.WriteLine($"Os numeros digitados foram {numero1}, {numero2}, {numero3}, {numero4} e {numero5} e sua soma é {soma}.");
+        }
+    }
+}
